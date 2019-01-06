eee93bfe944881ef41910cfa10014ca20c30925b
Maary <960500366@qq.com>
2019-01-06 14:52:24 +0800

    test tow functions

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 from flask import Flask
+'''for test'''
+from flask import request
+from flask import make_response
 
 app = Flask(__name__)
 
@@ -6,6 +9,23 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+
+
+@app.route('/test')
+def test_req():
+    user_agent = request.headers.get('User-Agent')
+    return '<p>Your browser is %s</p>' % user_agent
+
+
+@app.route('/test/response')
+def test_rsp():
+    response = make_response('<h1> this document is carries a cookie</h1>')
+    response.set_cookie('cookie1', 'qwe')
+    return response
+
+'''
+
+'''
 
 
 if __name__ == '__main__':
